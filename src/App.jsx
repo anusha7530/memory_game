50010c02a4edcb46098b19284a8efccde368490d
anusha7530 <agarwalanusha13@gmail.com>
2025-05-16 16:12:04 +0530

    Added game component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import MemoryGame from "./components/memory-game";
 
-const App = () => {
-  return (
-    <div>
-      hii
-    </div>
-  )
+function App() {
+  return <MemoryGame />;
 }
 
-export default App
+export default App;
